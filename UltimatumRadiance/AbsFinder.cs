b0577b8c9f03693c6e2cc5e5494e74835ae4db7f
Alek Fleischer <thecrazyal02@gmail.com>
2018-10-10 23:35:42 -0700

    Beam sweeps now correctly cover larger area in the platform phase

--- UltimatumRadiance/AbsFinder.cs
+++ UltimatumRadiance/AbsFinder.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
 using Logger = Modding.Logger;
 
 namespace UltimatumRadiance
@@ -12,10 +13,12 @@ namespace UltimatumRadiance
 
         private PlayMakerFSM _beamsweepercontrol;
         private PlayMakerFSM _beamsweeper2control;
+        private PlayMakerFSM _absattackchoices;
 
         private bool _cloned;
         private bool _assigned;
         private bool _returned;
+        private bool _arena2set;
 
         private void Start()
         {
@@ -28,6 +31,7 @@ namespace UltimatumRadiance
                 _cloned = false;
                 _assigned = false;
                 _returned = false;
+                _arena2set = false;
                 _abs = GameObject.Find("Absolute Radiance");
             }
 
@@ -53,6 +57,7 @@ namespace UltimatumRadiance
                 _beamsweeper2.AddComponent<BeamSweeperClone>();
                 _beamsweepercontrol = _beamsweeper.LocateMyFSM("Control");
                 _beamsweeper2control = _beamsweeper2.LocateMyFSM("Control");
+                _absattackchoices = _abs.LocateMyFSM("Attack Choices");
             }
 
             else if (_beamsweeper2control != null)
@@ -69,6 +74,25 @@ namespace UltimatumRadiance
                             break;
                     }
 
+                }
+                if ((_absattackchoices.FsmVariables.GetFsmInt("Arena").Value == 2) && !_arena2set)
+                {
+                    Logger.Log("[Ultimatum Radiance] Starting Phase 2");
+                    _arena2set = true;
+
+                    _beamsweepercontrol.GetAction<SetPosition>("Beam Sweep L", 3).x = 89;
+                    _beamsweepercontrol.GetAction<iTweenMoveBy>("Beam Sweep L", 5).vector = new Vector3 (-50, 0, 0);
+                    _beamsweepercontrol.GetAction<iTweenMoveBy>("Beam Sweep L", 5).time = 5;
+                    _beamsweepercontrol.GetAction<SetPosition>("Beam Sweep R", 4).x = 32.6f;
+                    _beamsweepercontrol.GetAction<iTweenMoveBy>("Beam Sweep R", 6).vector = new Vector3(50, 0, 0);
+                    _beamsweepercontrol.GetAction<iTweenMoveBy>("Beam Sweep R", 6).time = 5;
+
+                    _beamsweeper2control.GetAction<SetPosition>("Beam Sweep L", 2).x = 89;
+                    _beamsweeper2control.GetAction<iTweenMoveBy>("Beam Sweep L", 4).vector = new Vector3(-50, 0, 0);
+                    _beamsweeper2control.GetAction<iTweenMoveBy>("Beam Sweep L", 4).time = 10;
+                    _beamsweeper2control.GetAction<SetPosition>("Beam Sweep R", 3).x = 32.6f;
+                    _beamsweeper2control.GetAction<iTweenMoveBy>("Beam Sweep R", 5).vector = new Vector3(50, 0, 0);
+                    _beamsweeper2control.GetAction<iTweenMoveBy>("Beam Sweep R", 5).time = 5;
                 }
             }
         }
